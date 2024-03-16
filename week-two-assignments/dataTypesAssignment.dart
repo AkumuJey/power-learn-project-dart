8dc3f09236356c4c3a9c64a3042cb775d3890206
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-03-16 21:22:12 +0300

    Completed function assignment

---
+++ week-two-assignments/dataTypesAssignment.dart
@@ -0,0 +1,24 @@
+void main() {
+  // Integer data type
+  int age = 20;
+  print("Age: $age");
+  // double data type that covers decimals
+  double height = 170.12;
+  print("Height: $height");
+  // String data type
+  String name = "Joseph Akumu";
+  print("Name: $name");
+  // List of integers
+  List<int> scores = [85, 89, 99, 90];
+  print("Scores: $scores");
+// Map to store list of planets as key value pairs.
+  Map<String, String> planets = {
+    "first": "Mercury",
+    "second": "Venus",
+    "third": "Earth",
+    "fourth": "Mars",
+    "fifth": "Jupiter"
+  };
+
+  print(planets["third"]);
+}
